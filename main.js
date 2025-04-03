4b1665b6d04046bfe798e891041f719b70d94729
vintiyannick <yannickvinti@gmail.com>
2025-04-03 16:45:25 +0200

    asynchrone Funktion zum Laden von JSON Daten

--- main.js
+++ main.js
@@ -21,8 +21,17 @@ L.tileLayer('https://mapsneu.wien.gv.at/basemap/bmapgrau/normal/google3857/{z}/{
 let marker = L.marker([stephansdom.lat, stephansdom.lng]).addTo(map);
 marker.bindPopup(stephansdom.title).openPopup();
 
-
 // Maßstab
 L.control.scale({
     imperial: false,
 }).addTo(map);
+
+// Sehenswürdigkeiten Standorte Wien
+async function loadSights(url) {
+     //console.log(url);
+    let response = await fetch(url);
+    let jsondata = await response.json();
+    //console.log(jsondata);
+    L.geoJSON(jsondata).addTo(map);
+}
+loadSights("https://data.wien.gv.at/daten/geo?service=WFS&request=GetFeature&version=1.1.0&typeName=ogdwien:SEHENSWUERDIGOGD&srsName=EPSG:4326&outputFormat=json");
